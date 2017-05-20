e7e58b7e48f8fe2df40d7bc1c72d69222520ffc0
Cloud User <ec2-user@ip-10-0-0-159.ec2.internal>
2017-05-20 17:15:25 -0400

    third commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,20 +1,38 @@
-yum -y update
- yum -y install java-1.8.0-openjdk-devel.x86_64
- yum -y install wget
- yum -y install git
+#
+# Cookbook:: jenkins1
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
 
- yum update –y
- wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkinsci.org/redhat/jenkins.repo
- rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
- yum install jenkins -y
-  
- mkdir -p /opt/maven
- cd /opt/maven
- wget http://mirrors.advancedhosters.com/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
- tar xvf apache-maven-3.5.0-bin.tar.gz
- chown -R jenkins:jenkins /opt/maven
+execute 'yum_update' do
+  command 'yum -y update'
+end
+
+yum_package 'java-1.8.0-openjdk-devel.x86_64' do
+  action :install
+end
+
+yum_package 'wget' do
+  action :install
+end
+
+execute 'wget_repo' do
+  command 'wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo'
+end
+
+execute 'rpm_import' do
+  command 'rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key'
+end
+
+execute 'install_jenkins' do
+  command 'yum -y install jenkins'
+end
+
+yum_package 'jenkins' do
+  action :install
+end
 
 service 'jenkins' do
-   support status: true, restart: true, reload:true
-   action [:enable, :start]
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
 end
